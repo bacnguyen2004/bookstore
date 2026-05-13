1d3058c755a05cb3d936f23db468e2e269a6ac32
bacnguyen2004 <nguyenvanbac2k4@gmail.com>
2026-05-13 22:39:32 +0700

    update decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,13 +1,16 @@
 from django.shortcuts import redirect
+from functools import wraps
 
 def admin_required(view_func):
 
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
 
         if request.user.is_authenticated:
 
-            if request.user.customer.role == 'admin':
+            customer = getattr(request.user, 'customer', None)
 
+            if customer and customer.role == 'admin':
                 return view_func(
                     request,
                     *args,
